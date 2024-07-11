ef8d5726e310088e206768c98449a4c0ae1b30a1
bhli168 <935754747@qq.com>
2024-07-11 20:00:01 +0800

    Add files via upload

---
+++ Section IV-C/MDDConv_slim_sub.py
@@ -0,0 +1,90 @@
+from torch_geometric.nn.conv import MessagePassing
+from torch.nn import Sequential as Seq, Linear as Lin, Dropout, LeakyReLU, ReLU, Sigmoid, BatchNorm1d as BN
+import numpy as np                         # import numpy
+import torch
+import torch.nn as nn
+import torch.nn.functional
+
+# class MDDSlimLinear(nn.Linear):
+#     def __init__(self, in_slimfeature, out_slimfeature, max_dim, bias=True):
+#         super(MDDSlimLinear, self).__init__(max_dim, out_slimfeature, bias=bias)
+#         self.in_slimfeature = in_slimfeature
+#         self.out_slimfeature = out_slimfeature
+#         self.max_dim = max_dim
+#
+#     def forward(self, input):
+#         weight = self.weight[:, :self.in_slimfeature]
+#         bias = self.bias
+#         return nn.functional.linear(input, weight, bias)
+
+
+
+def MLP(channels, batch_norm=True):
+    return Seq(*[
+        Seq(Lin(channels[i - 1], channels[i]),BN(channels[i]), LeakyReLU())#,
+        for i in range(1, len(channels))
+    ])
+
+class IGConv(MessagePassing):
+    def __init__(self, mlp1, mlp2, **kwargs):
+        super(IGConv, self).__init__(aggr='add', **kwargs)
+        self.mlp1 = mlp1
+        self.mlp2 = mlp2
+        # self.reset_parameters()
+
+    def reset_parameters(self):
+        reset(self.mlp1)
+        reset(self.mlp2)
+
+    def update(self, aggr_out, x):
+        if isinstance(x, tuple):
+            tmp = torch.cat([x[1], aggr_out], dim=1)
+            comb = self.mlp2(tmp)
+            comb = (comb + x[1]) / 2
+        else:
+            tmp = torch.cat([x, aggr_out], dim=1)
+            comb = self.mlp2(tmp)
+            comb = (comb + x) / 2
+
+        return comb
+
+    def forward(self, x, edge_index, edge_attr):
+        return self.propagate(edge_index, x=x, edge_attr=edge_attr)
+
+    def message(self, x_i, x_j, edge_attr):
+        #tmp = torch.cat([x_j, edge_attr], dim=1)
+        tmp = torch.mul(x_j, edge_attr)
+        agg = self.mlp1(tmp)
+        return agg
+
+    def __repr__(self):
+        return '{}(nn={})'.format(self.__class__.__name__, self.mlp1, self.mlp2)
+
+
+class MDDSlimNet_AP(torch.nn.Module):
+    def __init__(self, n_ch1):
+        super(MDDSlimNet_AP, self).__init__()
+        #self.slim_mlp = MDDSlimLinear(n_ch2, 128, max_dim)
+        #self.mlp1 = MLP([n_ch2+sub1, 128, 256])
+        self.mlp1 = MLP([384, 512])
+        self.mlp2 = MLP([n_ch1+512, 2048])
+        self.mlp2 = Seq(*[self.mlp2, Seq(Lin(2048, n_ch1), BN(n_ch1)),LeakyReLU()])
+        self.conv = IGConv(self.mlp1, self.mlp2)
+
+    def forward(self, x0, edge_index, edge_attr):
+        out = self.conv(x=x0, edge_index=edge_index, edge_attr=edge_attr)
+        return out
+
+class MDDSlimNet_MS(torch.nn.Module):
+    def __init__(self, n_ch1):
+        super(MDDSlimNet_MS, self).__init__()
+            # self.slim_mlp = MDDSlimLinear(n_ch2, 128, max_dim)
+            # self.mlp1 = MLP([n_ch2+sub1, 128, 256])
+        self.mlp1 = MLP([512, 1024])
+        self.mlp2 = MLP([n_ch1 + 1024, 2048])
+        self.mlp2 = Seq(*[self.mlp2, Seq(Lin(2048, n_ch1),BN(n_ch1)),LeakyReLU()])
+        self.conv = IGConv(self.mlp1, self.mlp2)
+
+    def forward(self, x0, edge_index, edge_attr):
+        out = self.conv(x=x0, edge_index=edge_index, edge_attr=edge_attr)
+        return out
